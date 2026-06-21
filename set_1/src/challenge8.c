695e6fd84290a6e993161a6f7d1cc1178587b798
unai <unai@unainet.net>
2026-06-21 19:37:12 +0200

    Challenge 8 finished

---
+++ set_1/src/challenge8.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "convert.h"
+#include "print.h"
+
+#define LINE_SIZE 60
+
+static int check_repeated_blocks(uint8_t* bytes,size_t len,size_t blksiz)
+{
+  size_t blocks = len / blksiz;
+  size_t i,j;
+  int score = 0;
+
+  for( i = 0; i < blocks-1; i++ ) {
+    for( j = i+1; j < blocks-1;j++ ) {
+      if( 0 == memcmp(&bytes[i*blksiz],&bytes[j*blksiz],blksiz) ) {
+        score++;
+      }
+    }
+  }
+  return score;
+}
+
+int main(int argc,char** argv)
+{
+  FILE* file;
+
+  if( argc < 2 ) {
+    printf("I need a file to parse\n");
+    return -1;
+  }
+  file = fopen(argv[1],"r");
+  if( NULL == file ) {
+    printf("Failure opening the file\n");
+    return -2;
+  }
+  
+  size_t llen; // line len
+  char* line=NULL;
+  size_t lineno=0;
+  uint8_t* raw_bytes = NULL;
+
+  while(getline(&line,&llen,file) > 0 ) {
+    lineno++;
+    line[strcspn(line, "\n")] = '\0';
+    llen = strlen(line);
+    size_t bytesno = str2bytes(line,&raw_bytes);
+    //print_bytes(stdout,raw_bytes,bytesno,"buf:");
+    //printf("bytesno=%zu llen=%zu\n",bytesno,llen);
+    int score = check_repeated_blocks(raw_bytes,bytesno,16);
+    if( score > 0 ) {
+      printf("Score for line %zu is %d\n",lineno,score);
+    }
+    free(line);
+    line = NULL;
+    llen = 0;
+    free(raw_bytes);
+    raw_bytes = NULL;
+  }
+  
+   
+end:
+  fclose(file);
+  return 0;
+}
